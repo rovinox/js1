33baef7ac33901688fe2634017ab9ae6253736df
neaj mahmud <neajmahmud230@gmail.com>
2020-10-08 20:21:44 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+//////////////////PROBLEM 1////////////////////
+
+//Create a variable called myName that is a string data type
+
+//////////////////PROBLEM 2////////////////////
+
+//Create a variable called myAge that is a number data type
+
+//////////////////PROBLEM 3////////////////////
+
+//Create a variable called lovesCode that is a boolean data type
+
+//////////////////PROBLEM 4////////////////////
+
+//Create a variable called greatestFear that is undefined because we fear nothing
+
+//////////////////PROBLEM 5////////////////////
+
+//Create a variable called games that is null because we are just starting out
+
+//////////////////PROBLEM 6////////////////////
+
+//Create a function declaration called greeting that
+//accepts name as its only parameter.
+//greeting should return the string "Hello, "
+//plus the value of the name parameter.
+
+//////////////////PROBLEM 7////////////////////
+
+//Rewrite the function greeting as a function expression.
+//Name it newGreeting.
+
+//////////////////PROBLEM 8////////////////////
+
+//Rewrite the function greeting as an arrow function.
+//Name it finalGreeting.
+
+//////////////////PROBLEM 9////////////////////
+
+//Create an array called groceries with the values
+//"apples", "milk", "eggs", "bread"
+
+//////////////////PROBLEM 10////////////////////
+
+//Create an object saved to the variable dog.
+//The dog object should have the following properties:
+//name (a string), color (a string), age (a number),
+//and goodBoy (a boolean).
+
+//...access the dog's name from the object and assign it to a
+//variable called myPet.
+
+//////////////////PROBLEM 11////////////////////
+
+//Write a function called nameCheck that takes in a name parameter.
+//nameCheck should check if the name equals 'Steven'. If it does,
+// return 'What is up Steven?'
+//If the name parameter is Bryan, return 'Hey Bryan!'
+// If the name parameter is anything else, return 'Cool name, NAMEPARAM'
+// with NAMEPARAM being the name parameter being passed in
+
+//////////////////PROBLEM 12////////////////////
+
+// Create a function called add that takes in two parameters
+// that will be numbers.
+// The add function should return the two parameters added together
+
+//Now invoke add, passing in the numbers 3 and 4
+//storing the result in the variable mathSum.
+
+//////////////////PROBLEM 13////////////////////
+
+//Write a function called faveColorFinder that takes in one parameter called color
+// that will be a string.
+// If the passed in color equals 'red', return 'red is a great color'
+// If the passed in color equals 'green', return 'green is a solid favorite color'
+// If the passed in color equals 'black', return 'so trendy'
+// Otherwise, you should return the string 'you need to evaluate your favorite color choice'
